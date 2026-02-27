0301b8d4a32dea82785d2c976f8220f26a0c3bff
hena8x8 <Zhovtko1984@mail.com>
2026-02-27 11:45:30 +0100

    zd2

---
+++ oop0/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Point p = new Point();
+
+        p.x = 5F;
+        p.y = 7.5F;
+
+        System.out.println(p.toSvg());
+
+    }
+}
