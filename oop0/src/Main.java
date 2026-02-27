bbb12a66823767066407c8bd7074ee459f052532
hena8x8 <Zhovtko1984@mail.com>
2026-02-27 12:32:25 +0100

    zd4

--- oop0/src/Main.java
+++ oop0/src/Main.java
@@ -20,5 +20,35 @@ public class Main {
         s.q = p2;
 
         System.out.println(s.length());
+
+
+        Point p3 = new Point();
+        p3.x = 6;
+        p3.y = 13;
+        Point p4 = new Point();
+        p4.x = 5;
+        p4.y = 15;
+        Segment s2 = new Segment();
+        s2.p = p3;
+        s2.p = p4;
+
+        Point p5 = new Point();
+        p5.x = 9;
+        p5.y = 23;
+        Point p6 = new Point();
+        p6.x = 4;
+        p6.y = 11;
+        Segment s3 = new Segment();
+        s3.p = p5;
+        s3.p = p6;
+
+        Segment[] segments = new Segment[3];
+        segments [0] = s;
+        segments [1] = s2;
+        segments [2] = s3;
+
+        System.out.println(Segment.findMax(segments).p);
     }
+
+
 }
--- oop0/src/Segment.java
+++ oop0/src/Segment.java
@@ -4,3 +4,10 @@ public class Segment {
         return (float) Math.hypot(p.x - q.x, p.y - q.y );
     }
 }
+public static Segment findMax (Segment[] segments){
+    Segment maxSeg = segments[0];
+    for (Segment s : segments){
+        if(s.length() > maxSeg.length()) maxSeg = 5;
+    }
+    return maxSeg;
+}
