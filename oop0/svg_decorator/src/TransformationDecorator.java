5fc8082497b3ca747b04b2290699caa6eb2bf426
hena8x8 <Zhovtko1984@mail.com>
2026-03-20 12:17:23 +0100

    lb3 4

--- oop0/svg_decorator/src/TransformationDecorator.java
+++ oop0/svg_decorator/src/TransformationDecorator.java
@@ -3,6 +3,12 @@ import java.util.Locale;
 public class TransformationDecorator extends ShapeDecorator{
     private String transform;
 
+    public TransformationDecorator(Shape decoratedShape, Vec2 translatiom){
+        super(decoratedShape);
+        Builder builder = new Builder().translate(translatiom).rotate(45),scale(2.0);
+        this.transform = builder.build();
+    }
+
     @Override
     public String toSvg() {
         return this.decoratedShape.toSvg().replace("/>",
@@ -26,18 +32,13 @@ public class TransformationDecorator extends ShapeDecorator{
                         "translate(%f %f",
                         translatiom.x(), translatiom.y());
             }
+            return result;
         }
 
-        public TransformationDecorator(Shape decoratedShape, Vec2 translatiom){
-            super(decoratedShape);
-            Builder builder = new Builder().translate(translatiom).rotate(45),scale(2.0);
-            this.transform = builder.build();
-        }
 
-        public Builder translate(Vec2, translatiom){
-            this.translatiom = translatiom;
-            return this;
-        }
+
+
+
 
     }
 
