a9deb92eb562b64c21f73a9b02c18bb9fb3a9665
hena8x8 <Zhovtko1984@mail.com>
2026-03-27 10:39:39 +0100

    lb5 zd1

---
+++ oop0/genealogy/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person(
+                "Mike",
+                "Tyson",
+                LocalDate.of(1966, 5, 18)
+        ));
+        people.add(new Person(
+                "Anna",
+                "Nowak",
+                LocalDate.of(2002, 4, 10)
+        ));
+        people.add(new Person(
+                "Muhammad",
+                "Ali",
+                LocalDate.of(1944, 1, 2)
+        ));
+
+        System.out.println(people);
+
+    }
+}
